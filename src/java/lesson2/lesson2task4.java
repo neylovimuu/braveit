a696c80a6ed8b7d658853d074227058da7e9c348
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-06-26 14:32:45 +0300

    додав код щоб зменшити кількість знаків в фарингейтах після коми.

--- src/java/lesson2/lesson2task4.java
+++ src/java/lesson2/lesson2task4.java
@@ -1,10 +1,16 @@
 package lesson2;
 
+import java.text.DecimalFormat;
+
 public class lesson2task4 {
     public static void main(String[] args) {
         double F = 35.5;
         double C = convertToCelsius(F);
-        System.out.println(F + " F " + C + " C ");
+
+        DecimalFormat df = new DecimalFormat("#.##");        // виконав завдання і захотілось мені зменшити кількість знаків в фарингейтах після коми
+        String formatC = df.format(C);
+
+        System.out.println(F + " F " + formatC + " C ");
 
     }
         public static double convertToCelsius(double degreeFahrenheit) {
