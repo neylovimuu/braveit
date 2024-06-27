f6def93445ad707d3827c9413b2e9e3325ef4f7b
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-06-27 15:39:34 +0300

    Видалив коментар з коду.

--- src/java/lesson2/TaskFour.java
+++ src/java/lesson2/TaskFour.java
@@ -7,7 +7,7 @@ public class TaskFour {
         double faringate = 35.5;
         double celsius = convertToCelsius(faringate);
 
-        DecimalFormat df = new DecimalFormat("#.##");        // виконав завдання і захотілось мені зменшити кількість знаків в фарингейтах після коми
+        DecimalFormat df = new DecimalFormat("#.##");
         System.out.println(faringate + " F = " + df.format(celsius) + " C ");
     }
 
