270118177aeeb2fb1ef6a76386c7e06b49b9dbca
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-07-02 16:38:24 +0300

    Переписав, але не впевнений що правильно. Перевірь будь ласка.

--- src/java/lesson3/TaskTwo.java
+++ src/java/lesson3/TaskTwo.java
@@ -4,15 +4,16 @@ public class TaskTwo {
     public static void main(String[] args) {
         int dayNumber = 22;
         System.out.println("Введено число " + dayNumber);
-        switch (dayNumber) {
-                case 1 -> System.out.print("Monday");
-                case 2 -> System.out.print("Tuesday");
-                case 3 -> System.out.print("Wednesday");
-                case 4 -> System.out.print("Thursday");
-                case 5 -> System.out.print("Friday");
-                case 6 -> System.out.print("Saturday");
-                case 7 -> System.out.print("Sunday");
-                default -> System.out.print("Щось пішло не так, ти впевнений що правильно вказав номер дня?");
-        }
+        String d1 = switch (dayNumber) {
+                case 1 -> "Monday";
+                case 2 -> "Tuesday";
+                case 3 -> "Wednesday";
+                case 4 -> "Thursday";
+                case 5 -> "Friday";
+                case 6 -> "Saturday";
+                case 7 -> "Sunday";
+                default -> "Щось пішло не так, ти впевнений що правильно вказав номер дня?";
+        };
+        System.out.println(d1);
     }
 }
