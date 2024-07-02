fe9fb559352f02dd2ba27609c65600c7659ae6c3
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-07-02 13:45:39 +0300

    Напиcав програму на Java для друку суми (додавання), множення, віднімання, ділення та залишку двох чисел. Створив нову гілку під це завдання. Закомітив. Запушив

--- src/java/lesson3/TaskOne.java
+++ src/java/lesson3/TaskOne.java
@@ -2,13 +2,13 @@ package lesson3;
 
 public class TaskOne {
     public static void main(String[] args){
-    double y = 4.0;
-    double w = 6.0;
-    System.out.println(y+w);
-    System.out.println(y*w);
-    System.out.println(y-w);
-    System.out.println(y/w);
-    System.out.println(y%w);
-    System.out.println(++y%w);
+        double y = 4.0;
+        double w = 6.0;
+        System.out.println(y+w);
+        System.out.println(y*w);
+        System.out.println(y-w);
+        System.out.println(y/w);
+        System.out.println(y%w);
+        System.out.println(++y%w);
     }
 }
