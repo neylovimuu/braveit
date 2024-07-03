3a01ff463b109c006f5bc62278d969dfb620e79e
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-07-03 12:39:10 +0300

    Ще раз підправив вивід, виводило парні, тепер все вірно виводить не парні до 200

--- src/java/lesson4/TaskOne.java
+++ src/java/lesson4/TaskOne.java
@@ -6,7 +6,7 @@ public class TaskOne {
         int count = 101;
         while (count <= 199) {
             System.out.println(count);
-            count = count + 1;
+            count = count + 2;
         }
         System.out.println("Все стопе, зупинись...");
     }
