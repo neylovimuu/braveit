d914bf74c6b9fb4cf41975df740be500dec4ed9b
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-07-03 09:37:16 +0300

    Змінив назву String s1 на більш описову result.

--- src/java/lesson3/TaskThree.java
+++ src/java/lesson3/TaskThree.java
@@ -3,13 +3,13 @@ package lesson3;
 public class TaskThree {
     public static void main(String[] args) {
         int month = 15;
-        String s1 = switch (month) {
+        String result = switch (month) {
                 case 1, 2, 12 -> "Winter";
                 case 3, 4, 5 -> "Spring";
                 case 6, 7, 8 -> "Summer";
                 case 9, 10, 11 -> "Fall";
                 default -> "Бро, рік складається з 12 місяців, не тупи";
         };
-        System.out.println(s1);
+        System.out.println(result);
     }
 }
