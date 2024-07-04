2caa5fde74634401bfb51b6cb33d7580459e25d8
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-07-04 09:43:41 +0300

    Програма для обчислення суми цифр цілого числа.Початок.

---
+++ src/java/lesson4/TaskEight.java
@@ -0,0 +1,15 @@
+package lesson4;
+//Напишіть програму для обчислення суми цифр цілого числа.
+import java.util.Scanner;
+
+public class TaskEight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть перше число:");
+        int firstNumber = scanner.nextInt();
+
+
+
+        }
+    }
+}
