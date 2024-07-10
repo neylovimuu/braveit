7255d3b3d913ce65645f381719b8b13fa3913e16
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-07-10 10:48:56 +0300

    Переназвав змінну

---
+++ src/java/lesson4/TaskOne.java
@@ -0,0 +1,13 @@
+packpackage lesson4;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        System.out.println("Кодиш, як бог, я в шоці, 4е заннятя,а ти вже таке виписуєш.Красень!");
+        int number = 101;
+        while (number <= 199) {
+            System.out.println(number);
+            number = number + 2;
+        }
+        System.out.println("Все стопе, зупинись...");
+    }
+}
