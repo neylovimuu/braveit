7639f7467d62d6f38fba71be1fd8fe0bfecd33f4
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-07-10 13:59:15 +0300

    Створив функцію, яка може перетворити рядок на число.

--- src/java/lesson5/TaskOne.java
+++ src/java/lesson5/TaskOne.java
@@ -1,20 +1,15 @@
 package lesson5;
 /**
- * Написати програму Java яка отримує n- чисел типу int як передані параметри. Порахувати їх суму,
- * результат вивести в консоль.
+ * Створити функцію, яка може перетворити рядок на число.
  */
 public class TaskOne {
     public static void main(String[] args) {
-        int [] myArray = {1, 5, 60};
-        int [] myArray1 = {1, 5, -60, 10};
-        System.out.println(sumOfElement(myArray));
-        System.out.println(sumOfElement(myArray1));
-    }
-    public static int sumOfElement(int[] array) {
-        int  result = 0;
-        for (int j : array) {
-            result += j;
-        }
-        return result ;
+        String myString = "12345"; // створив рядок
+        System.out.println(myString); // вивів в консоль сворений рядок
+
+        int number = Integer.parseInt(myString); // використав метод який перетворив рядок в число
+        System.out.println("Ціле число: " + number); // вивів в консоль перетворений рядок в число
+        System.out.println(number + 5); // Для наглядної різниці
+        System.out.println(myString + 5); // Для наглядної різниці
     }
 }
