cbf4488cfce10023cd7419fe3da5731867896913
Oleksii Riabchenko <neylovimuu17@gmail.com>
2024-07-11 17:06:34 +0300

    Після імпортів додав порожній рядок

--- src/java/lesson4/TaskFour.java
+++ src/java/lesson4/TaskFour.java
@@ -3,6 +3,7 @@ package lesson4;
 /**
  * Вивести парні числа від -100 до 200
  */
+
 public class TaskFour {
     public static void main(String[] args) {
         for(int number = -100; number <= 200; number += 2) {
